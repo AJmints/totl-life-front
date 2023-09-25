9bdd2870ee91aac7cd91c84220e2906d3d32964c
AJmints <alexjmints@gmail.com>
2023-09-24 23:51:02 -0500

    new branch set up

--- components/core/Header.tsx
+++ components/core/Header.tsx
@@ -5,11 +5,13 @@ export default function Header() {
 
     return (
         <div className="flex bg-gray-700 shadow-lg bg-shadow-900/90">
+            <div>
             <Image
             src={totlhome}
             alt=""
             className="w-auto ml-10 h-16 hover:bg-gray-600 hover:shadow-lg px-1 m-2 rounded-md duration-300"
             />
+            </div>
         </div>
     )
 }
