cfd5dee57986c65da186586425252fee89408542
AJmints <alexjmints@gmail.com>
2023-10-06 12:55:58 -0500

    tidying up temporary about section

--- app/about/current-initiative/page.tsx
+++ app/about/current-initiative/page.tsx
@@ -1,11 +1,30 @@
+import Image from "next/image"
+import riverView from "../../../public/images/river-view.jpg"
+
 export default function AboutInitiativePage() {
 
     return (
-        <div className="h-screen">
+        <div className="flex mb-5">
             <div className="flex justify-center">
                 <div className="mx-5">
-                <h1 className="text-center mt-20 bg-gray-100 p-5 rounded-md">River Cleanup 2024</h1>
-                <h1 className="text-center mt-5 bg-gray-100 p-5 rounded-md">&#39;River Cleanup 2024,&#39; embodies our philosophy of &#39;We don&#39;t do much, but when we do, it is to help.&#39; Join us as we roll up our sleeves and work together to protect and restore our local rivers. Together, we can make a significant impact on the environment and the communities that rely on these waterways</h1>
+                <div className="flex justify-center text-3xl">
+                <h1 className=" mt-20 bg-gray-100 p-5 rounded-md">River Cleanup 2024</h1>
+                </div>
+                <Image
+                    src={riverView}
+                    alt=""
+                    className="w-72 h-auto mx-auto mt-5 bg-gray-100 p-2 rounded-md"
+                />
+                <div className=" mt-5 bg-gray-100 p-5 rounded-md">
+                <h1>&#39;River Cleanup 2024,&#39; 
+                embodies our philosophy of &#39;We don&#39;t do much, but when we do, it is to help.&#39; 
+                Join us as we roll up our sleeves and work together to protect and restore our local rivers. 
+                Together, we can make a significant impact on the environment and the communities that rely 
+                on these waterways</h1><br/>
+                <p>The details for this event are 
+                pending, but once Totl finds its footing, we will start releasing details about our first 
+                ever group river clean up in 2024!</p>
+                </div>
                 </div>
             </div>
         </div>
--- app/about/meet-the-team/page.tsx
+++ app/about/meet-the-team/page.tsx
@@ -5,22 +5,38 @@ import secretary from "../../../public/images/secretary-of-the-treasury.jpg"
 export default function AboutTeamPage() {
 
     return (
-        <div className="">
+        <div className="mb-5">
             <div className="flex justify-center">
                 <div className="mx-5">
-                    <h1 className="text-center mt-20 bg-gray-100 p-5 rounded-md">Get to know the heart and soul of TOTL.life
+                    <div className="flex justify-center">
+                    <h1 className="text-center mt-10 text-2xl bg-gray-100 p-5 rounded-md">Get to know the heart and soul of TOTL.life
                     </h1>
-
+                    </div>
                     <div className="block sm:flex justify-around mt-5">
-                    <Image src={viceroy} alt="" className="w-52 h-auto bg-gray-500 p-2 rounded-full shadow-lg shadow-gray-800 hover:p-0 duration-300"></Image>
-                    <Image src={secretary} alt="" className="w-52 h-auto bg-gray-500 p-2 rounded-full shadow-lg shadow-gray-800 hover:p-0 duration-300"></Image>
+                    <div>
+                    <Image src={viceroy} alt="" className="w-52 h-auto mx-auto bg-gray-500 p-2 rounded-full shadow-lg shadow-gray-800 hover:p-0 duration-300"/>
+                    <p className="bg-gray-100 p-2 rounded-md text-lg text-center mt-2">Alex</p>
+                    </div>
+                    <div>
+                    <Image src={secretary} alt="" className="w-52 mt-2 h-auto mx-auto bg-gray-500 p-2 rounded-full shadow-lg shadow-gray-800 hover:p-0 duration-300"/>
+                    <p className="bg-gray-100 p-2 rounded-md text-lg text-center mt-2">Iley</p>
+                    </div>
                     </div>
 
-                    <div className="text-center my-5 bg-gray-100 p-5 rounded-md">
-                    <p>our incredible team of TOTLers. 
-                    We&#39;re not just a collective; we&#39;re a *Bale,* a close-knit community united by a shared commitment to Sustainable Stewardship.</p>
-                    <p>\- *Alex our Viceroy* (he/him):</p>
-                    <p>\- *Iley our Secretary of Treasury* (he/him):</p>
+                    <div className="text-center my-5 space-y-2 bg-gray-100 p-5 rounded-md">
+                    <p>Our incredible team of TOTLers. 
+                    We&#39;re not just a collective; we&#39;re a *Bale,* a close-knit community united by a 
+                    shared commitment to Sustainable Stewardship.</p>
+                    <p className="text-2xl">Alex, the Viceroy (he/him):</p>
+                    <p>Alex was raised in Missouri, and always had a strong love for nature. Growing up, he
+                    had exposure to many of the beautiful local, city, state, and national parks that MO 
+                    has to offer. He also got to visit some of the parks in the South Eastern part of the US. 
+                    His love for nature sparked a desire to get more involved in preserving the natural 
+                    beauty that we have left. That is what sparked the desire for Alex and Iley to create the 
+                    Totl.life website! We believe in the power of organizing and working towards a common 
+                    goal, and hope you do too! 
+                    </p>
+                    <p className="text-2xl">Iley, the Secretary of Treasury (he/him):</p>
                     <p>Iley is a native of Missouri and Oklahoma, where the rivers and lakes of the Midwest became the backdrop to many of
                     his childhood summers. Infused with a love for the outdoors from a young age, Iley felt a calling to ensure that the
                     pristine beauty he enjoyed would be preserved for future generations.
