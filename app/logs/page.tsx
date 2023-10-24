7713f82bcc3632c96619a0b03b835f3593eff37b
AJmints <alexjmints@gmail.com>
2023-10-23 23:54:52 -0500

    checking

--- app/logs/page.tsx
+++ app/logs/page.tsx
@@ -4,18 +4,6 @@ import LogLoadOut from "@/components/bales/LogLoadOut";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export const authCheck = async() => {
-    const infoCall = await fetch("/api/authCheck")
-    const status = await infoCall.json().catch((err) => {
-        console.log(err)
-    })
-    if (status.loggedIn) {
-        return true
-    } else {
-        return false
-    }
-}
-
 export default function BalesPage() {
 
     const [checkUser, setCheckUser] = useState<boolean>(false)
@@ -33,6 +21,18 @@ export default function BalesPage() {
         }
         userPresent()
     }, [])
+
+    const authCheck = async() => {
+        const infoCall = await fetch("/api/authCheck")
+        const status = await infoCall.json().catch((err) => {
+            console.log(err)
+        })
+        if (status.loggedIn) {
+            return true
+        } else {
+            return false
+        }
+    }
 
     return (
         <>
