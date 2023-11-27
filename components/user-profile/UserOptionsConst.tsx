937ff3ac4433e8c449dadcc88272aea47de1b0b7
AJmints <alexjmints@gmail.com>
2023-11-26 21:55:19 -0600

    check

--- components/user-profile/UserOptionsConst.tsx
+++ components/user-profile/UserOptionsConst.tsx
@@ -4,6 +4,7 @@ import UserOptions from "./UserOptions";
 import arrow from "../../public/icons/Arrow.png"
 import Image from "next/image";
 import { useState, useEffect } from 'react'
+import UserMain from "./UserMain";
 
 let USER_ID: string
 const URL = process.env.NEXT_PUBLIC_BACKEND_URL
@@ -59,7 +60,7 @@ export default function UserOptionsConst(props: any) {
                 <div className="">
                 <Image src={arrow} alt="Close" onClick={() => props.setUserDetailsToggle((prev: any) => !prev)} className="cursor-pointer relative z-20 bg-gray-300 p-2 right-8 rotate-180 rounded-md hover:shadow-lg drop-shadow-xl top-20 w-14 hover:bg-emerald-500 duration-300 hover:shadow-gray-800/80 hover:rotate-0" />
                 </div>
-                <UserOptions
+                <UserMain
                 logout={props.logout}
                 userPFP={props.userPFP}
                 setUserPFP={props.setUserPFP}
