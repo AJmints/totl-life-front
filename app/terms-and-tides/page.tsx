e40e9085e1c9810dc84ea30999a511ae86601def
asands1769 <asands1769@outlook.com>
2024-01-25 13:58:41 -0500

    Styling on terms/tides

--- app/terms-and-tides/page.tsx
+++ app/terms-and-tides/page.tsx
@@ -1,70 +1,38 @@
 'use client'
 
-import arrow from '../../../../../public/icons/Arrow.png'
-import { useState, useEffect } from "react"
-import { usePathname, useRouter } from "next/navigation"
-import { useRiverContext } from '@/app/context/RiverContextProvider'
-import Image from 'next/image'
-// import FollowJoinLog from '../../buttons/follow-join-log/FollowJoinLog'
-import Link from 'next/link'
-
 
 const TermsAndConditions = () => {
 return (<div className="mx-24 bg-gray-700/80 p-3 rounded-md shadow-lg shadow-gray-900/70">
         
-<p className="text-lg text-center">
+<p className="text-lg text-center font-extrabold">
 Terms and Tides: Navigating the River
 <br></br>
 <br></br>
 <br></br>
-
-Ahoy there, adventurous soul! Welcome to the whimsical waters of The River, where the current is strong, and the laughter flows freely. By embarking on this aquatic escapade, you agree to ride the waves with the following terms and tides!
+<p>Ahoy there, adventurous soul! Welcome to the whimsical waters of The River, where the current is strong, and the laughter flows freely. By embarking on this aquatic escapade, you agree to ride the waves with the following terms and tides!</p>
 <br></br>
+<p>1. Fishy Friendships:</p>
+<p>Befriend the river inhabitants, especially the wise old catfish who may offer life advice in exchange for corny jokes. Failure to make at least one aquatic friend may lead to fin-tastic penalties.</p>
 <br></br>
-1. Fishy Friendships:
+<p>2.Raft of Randomness:</p>
+<p>You may encounter a raft of randomness floating downstream. Hitch a ride, share a joke, or perform a river dance to join the festivities.</p>
 <br></br>
-
-Befriend the river inhabitants, especially the wise old catfish who may offer life advice in exchange for corny jokes. Failure to make at least one aquatic friend may lead to fin-tastic penalties.
-
+<p>3.Rapid Humor Rapids:</p>
+<p>Brace yourself for the rapid-fire delivery of jokes. Laughing too hard may result in uncontrollable snorting, which is perfectly acceptable and encouraged except on Tuesdays, we wouldn't want to wake Larry.</p>
 <br></br>
-
-2.Raft of Randomness:
+<p>4. Don't wake Larry:</p>
+<p>Don't wake Larry.</p>
 <br></br>
-
-You may encounter a raft of randomness floating downstream. Hitch a ride, share a joke, or perform a river dance to join the festivities.
+<p>5.Buoyant Banter Bylaws:</p>
+<p>Engage in buoyant banter at all times. Float with the current of conversation, and beware of eddy-induced awkward silences.</p>
 <br></br>
-
-3.Rapid Humor Rapids:
+<p>6.Flood of Fun Guarantee:</p>
+<p>In the unlikely event of a fun drought, please notify our Water Wit Department. We'll release a flood of humor to restore the jovial atmosphere.</p>
 <br></br>
-
-Brace yourself for the rapid-fire delivery of jokes. Laughing too hard may result in uncontrollable snorting, which is perfectly acceptable and encouraged except on Tuesdays, we wouldn't want to wake Larry.
+<p>7. Tidal Tickling:</p>
+<p>Tidal tickling sessions may occur unexpectedly. Prepare to be tickled by invisible water nymphs or ticklish trout. Resistance is futile.</p>
 <br></br>
-
-4. Don't wake Larry:
-<br></br>
-
-Don't wake Larry.
-<br></br>
-
-5.Buoyant Banter Bylaws:
-<br></br>
-
-Engage in buoyant banter at all times. Float with the current of conversation, and beware of eddy-induced awkward silences.
-<br></br>
-
-6.Flood of Fun Guarantee:
-<br></br>
-
-In the unlikely event of a fun drought, please notify our Water Wit Department. We'll release a flood of humor to restore the jovial atmosphere.
-<br></br>
-
-7. Tidal Tickling:
-<br></br>
-
-Tidal tickling sessions may occur unexpectedly. Prepare to be tickled by invisible water nymphs or ticklish trout. Resistance is futile.
-<br></br>
-
-These terms are as fluid as the river itself and may change course without notice. Please feel free to express any complaints by writing them down, placing the note in a bottle, and tossing it in the river.
+<p>These terms are as fluid as the river itself and may change course without notice. Please feel free to express any complaints by writing them down, placing the note in a bottle, and tossing it in the river.</p>
 </p>
 
 </div>)
