531d02318527d3de4a366826800dc4f0fe8cf549
AJmints <alexjmints@gmail.com>
2024-04-14 23:22:05 -0500

    Added title in BackpackViewer

--- components/user-profile/pageview-container/user-tab-history/backpack-container/back-pack-viewer/BackPackViewer.tsx
+++ components/user-profile/pageview-container/user-tab-history/backpack-container/back-pack-viewer/BackPackViewer.tsx
@@ -24,11 +24,16 @@ const BackPackViewer = (props: any) => {
         {props.packContents ? 
             <div>
                 <div className="flex flex-wrap gap-2">
+                <button className="bg-gray-400 hover:bg-emerald-500 duration-200 rounded-md p-2">Add New Pack</button>
                     <button className="bg-gray-400 hover:bg-emerald-500 duration-200 rounded-md p-2">All Gear</button>
-                    <button className="bg-gray-400 hover:bg-emerald-500 duration-200 rounded-md p-2">BackPack Config 1</button>
-                    <button className="bg-gray-400 hover:bg-emerald-500 duration-200 rounded-md p-2">BackPack Config 2</button>
+                    
+                    <button className="bg-gray-400 hover:bg-emerald-500 duration-200 rounded-md p-2">BackPack Config 1 Example</button>
+                    {/* Clicking All Gear shows all Gear
+                    Selecting Add New Pack goes to a form to make a backpack config
+                    Selecting Example Config shows all gear that is a part of that configuration + Title changes */}
                 </div>
                 <div className="mt-3">
+                <h1 className="text-2xl font-normal">Viewing All Gear</h1> {/* Title that changes */}
                 <p>Gear Display:</p>
                     <div className="grid grid-cols-2 md:grid-cols-3 gap-1 sm:gap-2">
                         {gearListDisplay}
