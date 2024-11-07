2941af0c42002b809cf8466e5ea64fda8300c2ca
ajmints <alexjmints@gmail.com>
2024-11-07 17:41:18 -0600

    event navigation set up, ready to start create event page

--- components/user-profile/pageview-container/user-tab-history/events-container/EventsContainer.tsx
+++ components/user-profile/pageview-container/user-tab-history/events-container/EventsContainer.tsx
@@ -2,10 +2,12 @@
 
 import { useState } from "react"
 import EventView from "./event-view/EventView"
+import CreateEventForm from "./create-event-form/CreateEventForm"
 
 const EventsContainer = () => {
 
     const [eventToggle, setEventToggle] = useState<boolean>(false)
+    const [createToggle, setCreateToggle] = useState<boolean>(false)
 
     const test = () => {
         console.time("Timer")
@@ -18,7 +20,7 @@ const EventsContainer = () => {
     }
 
     const t = (
-        <div className={"bg-gray-300 hover:border-emerald-500 border-gray-300 border-2 duration-200 " + "cursor-pointer p-1 md:p-2 flex justify-around rounded-md text-sm"}>
+        <div className={"bg-gray-300 hover:border-emerald-500 border-gray-300 border-4 duration-200 " + "cursor-pointer p-1 flex justify-between px-4 rounded-md text-sm"}>
             <div>
                 <div className="flex justify-center">
                     <p className="py-10 bg-gray-200 rounded-md">Type Image</p>
@@ -28,17 +30,21 @@ const EventsContainer = () => {
             <div className="text-right flex flex-col justify-center">
                 <p>Event Name</p>
                 <p>Hosted By</p>
-                <p>Date Year</p>
+                <p>Date Range</p>
+                <p>5 Invited</p>
             </div>
             <div className="flex gap-2 flex-col md:flex-row justify-center">
                 { true ? 
                     <>
                         <button>Accept</button>
                         <button>Decline</button>
+                        <button>Maybe</button> {/* Maybe has limited functionality in Events and can't contribute to food nor will thier gear be accounted for, but they can lend gear still */}
                     </>
                 :
                     <>
-                        <button>Or Delete/Edit</button>
+                        <p>Status: Accepted</p>
+                        <button>Delete/Edit</button> {/* If creator display these */}
+                        <button>Leave</button> {/* If user, provide this option */}
                     </>
                 }
             </div>
@@ -60,28 +66,66 @@ const EventsContainer = () => {
     
     return (
         <div>
-            <div className="p-2 bg-gray-400 rounded-md w-[30%] mb-2">
-                <p className=" font-medium text-4xl">Events Page</p>
-                <p>What needs to be stored at the top of the component tree and what needs its own calls?</p>
+            <div className="flex">
+                <p className="bg-gray-400 p-2 px-4 rounded-md mb-2 font-medium text-2xl sm:text-4xl">Events Page</p>
             </div>
             <div className="p-2 w-[100%] bg-gray-400 rounded-md text-gray-800">
                 <div className="p-1">
-                    <button  onClick={() => setEventToggle((prev) => !prev) } className="bg-yellow-400 p-1 px-2 rounded-md">{eventToggle ? "Back" : "Create Event"}</button>
+                    {eventToggle && <button  onClick={() => setEventToggle((prev) => !prev) } className="bg-yellow-400 p-1 px-2 rounded-md">Back</button>}
                 </div>
                 <div >
                     {eventToggle && <EventView />}
                     { !eventToggle && 
+                    
                     <div className="flex flex-col gap-2">
-                        <div className="flex flex-col gap-2">
-                            <h1 className="text-2xl font-light py-2 text-center bg-gray-300 rounded-md">Your Events</h1>
-                            <div className="flex gap-2">
-                                <input type="text" className="p-0.5 rounded-md" />
-                                <button>Search</button>
+                        { createToggle ?
+                            <>
+                            </>
+                        :
+                            <>
+                            <div className="flex flex-col gap-2">
+                                <h1 className="text-2xl font-light py-2 text-center bg-gray-300 rounded-md">Your Events</h1>
+                                <div className="flex gap-2 flex-col sm:flex-row sm:justify-around">
+                                    <div className="flex justify-end">
+                                    <button  onClick={() => setCreateToggle((prev) => !prev) } className="bg-yellow-400 p-1 px-2 rounded-md">Create Event</button>
+                                    </div>
+                                    
+                                    <div className="flex justify-end gap-2">
+                                        <input type="text" className="p-0.5 rounded-md" />
+                                        <button className="bg-gray-300 p-1 px-2 rounded-md">Search</button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="bg-gray-500 rounded-md h-[30rem] p-2 md:p-4 flex flex-col gap-2 overflow-y-scroll scroll-track scroll-w scroll-handle">
-                            {loop}
-                        </div>
+
+                            <div className="flex gap-2 justify-between p-1 rounded-md bg-gray-500">
+                                <button className="bg-emerald-400 p-1 px-2 rounded-md">Events you created</button>
+                                <button className="bg-emerald-400 p-1 px-2 rounded-md">Events Invited to</button>
+                                <button className="bg-emerald-400 p-1 px-2 rounded-md">Drafts</button>
+                            </div>
+                            </>
+                        }
+                        
+                        { createToggle ? 
+                            <>
+                            <div className="p-1">
+                                <button  onClick={() => setCreateToggle((prev) => !prev) } className="bg-yellow-400 p-1 px-2 rounded-md">Back</button>
+                            </div>
+                                <CreateEventForm />
+                            </>
+                            :
+                            <div className="bg-gray-500 rounded-md h-[30rem] p-2 md:p-4 flex flex-col gap-2 overflow-y-scroll scroll-track scroll-w scroll-handle">
+                            
+                                { arr.length === 0 ? 
+                                <div className="bg-gray-400 p-10 text-center rounded-md">
+                                    <p className="text-2xl">You Have no events yet</p>
+                                </div>
+                                :
+                                <>
+                                    {loop}
+                                </>
+                                }
+                            </div>
+                        }
                     </div>
                     }
                 </div>
---
+++ components/user-profile/pageview-container/user-tab-history/events-container/create-event-form/CreateEventForm.tsx
@@ -0,0 +1,13 @@
+
+
+const CreateEventForm = () => {
+
+
+    return (
+        <div>
+            <p>Create Event</p>
+        </div>
+    )
+}
+
+export default CreateEventForm
