322cd2edb2c51ba04fd706bc016ee1739be28f4a
ajmints <alexjmints@gmail.com>
2024-11-07 19:10:08 -0600

    Create events beginning mock

--- components/user-profile/pageview-container/user-tab-history/events-container/create-event-form/CreateEventForm.tsx
+++ components/user-profile/pageview-container/user-tab-history/events-container/create-event-form/CreateEventForm.tsx
@@ -4,8 +4,22 @@ const CreateEventForm = () => {
 
 
     return (
-        <div>
+        <div className="bg-gray-300 p-2 rounded-md">
             <p>Create Event</p>
+            <p>Event Name:</p>
+            <p>Public or Private</p>
+            <p>Park Name</p>
+            <p>Date Range</p>
+            <p>Event Special Notes 100 char limit</p>
+            <br/><br/><br/>
+            <p>Make Recommended Gear list?</p>
+            <p>A list with all gear images all selected</p>
+            <p>You can remove a highlighted item type</p>
+            <p>Finish when all items required are highlighted</p>
+            <br/><br/><br/>
+            <p>Recommended Meals?</p>
+            <br/><br/><br/>
+            <p>Friends List</p>
         </div>
     )
 }
